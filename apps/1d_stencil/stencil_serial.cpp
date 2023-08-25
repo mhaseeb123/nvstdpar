42c2e862a8ee4ffbb51114b46421f088a70394c6
Weile Wei <lokwei9@gmail.com>
2023-08-25 13:36:10 -0700

    restart actions

--- apps/1d_stencil/stencil_serial.cpp
+++ apps/1d_stencil/stencil_serial.cpp
@@ -32,6 +32,7 @@ double k = 0.5;      // heat transfer coefficient
 double dt = 1.;      // time step
 double dx = 1.;      // grid spacing
 int test = 42;
+int anothertest = 42;
 
 ///////////////////////////////////////////////////////////////////////////////
 //[stepper_1
