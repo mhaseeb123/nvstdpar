1cd0cf17ef8b87ec2043a8c4a798f92f4b2ec846
hcq9102 <chuanqiuhe@gmail.com>
2023-09-19 23:33:31 -0700

    clang format

--- apps/choleskey/choleskey_serial.cpp
+++ apps/choleskey/choleskey_serial.cpp
@@ -5,61 +5,56 @@
 
 using namespace std;
 
-template<typename T>
-void Cholesky_Decomposition(std::vector<T> & vec, int n)
-{
-    int lower[n][n];
-    memset(lower, 0, sizeof(lower));
+template <typename T>
+void Cholesky_Decomposition(std::vector<T>& vec, int n) {
+  int lower[n][n];
+  memset(lower, 0, sizeof(lower));
 
-    using view_2d = std::extents<T, std::dynamic_extent, std::dynamic_extent>;
-    auto matrix = std::mdspan<T, view_2d, std::layout_right>(vec.data(), n,n);
- 
-    // Decomposing a matrix into Lower Triangular
-    for (int i = 0; i < matrix.extent(0); i++) {
-        for (int j = 0; j <= i; j++) {
-            int sum = 0;
- 
-            if (j == i) // summation for diagonals
-            {
-                for (int k = 0; k < j; k++)
-                    sum += pow(lower[j][k], 2);
+  using view_2d = std::extents<T, std::dynamic_extent, std::dynamic_extent>;
+  auto matrix = std::mdspan<T, view_2d, std::layout_right>(vec.data(), n, n);
 
-                lower[j][j] = sqrt(matrix(i,j) -
-                                        sum);                      
-            } else {
- 
-                // Evaluating L(i, j) using L(j, j)
-                for (int k = 0; k < j; k++)
-                    sum += (lower[i][k] * lower[j][k]);
-                lower[i][j] = (matrix(i,j) - sum) /
-                                      lower[j][j];
-            }
-        }
+  // Decomposing a matrix into Lower Triangular
+  for (int i = 0; i < matrix.extent(0); i++) {
+    for (int j = 0; j <= i; j++) {
+      int sum = 0;
+
+      if (j == i)  // summation for diagonals
+      {
+        for (int k = 0; k < j; k++)
+          sum += pow(lower[j][k], 2);
+
+        lower[j][j] = sqrt(matrix(i, j) - sum);
+      } else {
+
+        // Evaluating L(i, j) using L(j, j)
+        for (int k = 0; k < j; k++)
+          sum += (lower[i][k] * lower[j][k]);
+        lower[i][j] = (matrix(i, j) - sum) / lower[j][j];
+      }
     }
- 
-    // Displaying Lower Triangular and its Transpose
-    cout << setw(6) << " Lower Triangular" 
-         << setw(30) << "Transpose" << endl;
-    for (int i = 0; i < n; i++) {
-         
-        // Lower Triangular
-        for (int j = 0; j < n; j++)
-            cout << setw(6) << lower[i][j] << "\t";
-        cout << "\t";
-         
-        // Transpose of Lower Triangular
-        for (int j = 0; j < n; j++)
-            cout << setw(6) << lower[j][i] << "\t";
-        cout << endl;
-    }
+  }
+
+  // Displaying Lower Triangular and its Transpose
+  cout << setw(6) << " Lower Triangular" << setw(30) << "Transpose" << endl;
+  for (int i = 0; i < n; i++) {
+
+    // Lower Triangular
+    for (int j = 0; j < n; j++)
+      cout << setw(6) << lower[i][j] << "\t";
+    cout << "\t";
+
+    // Transpose of Lower Triangular
+    for (int j = 0; j < n; j++)
+      cout << setw(6) << lower[j][i] << "\t";
+    cout << endl;
+  }
 }
- 
-// Driver Code for testing
-int main()
-{
-    constexpr int n = 3;
-    std::vector<int> v{4, 12, -16,12, 37, -43, -16, -43, 98 };
-    Cholesky_Decomposition(v, n);
 
-    return 0;
+// Driver Code for testing
+int main() {
+  constexpr int n = 3;
+  std::vector<int> v{4, 12, -16, 12, 37, -43, -16, -43, 98};
+  Cholesky_Decomposition(v, n);
+
+  return 0;
 }
--- apps/choleskey/choleskey_stdpar.cpp
+++ apps/choleskey/choleskey_stdpar.cpp
@@ -2,87 +2,77 @@
 #include "argparse/argparse.hpp"
 #include "commons.hpp"
 
-#include <experimental/mdspan>
-#include <execution> // Include the execution header
-#include <vector>
-#include <iostream>
 #include <algorithm>
+#include <execution>  // Include the execution header
+#include <experimental/mdspan>
+#include <iostream>
 #include <numeric>
+#include <vector>
 
 using namespace std;
 
 struct Multiply {
-    int operator()(int a, int b) const {
-        return a * b;
-    }
+  int operator()(int a, int b) const { return a * b; }
 };
 
+template <typename T>
+void Cholesky_Decomposition(std::vector<T>& vec, int n) {
+  std::vector<std::vector<T>> lower(n, std::vector<T>(n, 0));
 
-template<typename T>
-void Cholesky_Decomposition(std::vector<T> & vec, int n)
-{
-    std::vector<std::vector<T>> lower(n, std::vector<T>(n, 0));
-    
+  using view_2d = std::extents<T, std::dynamic_extent, std::dynamic_extent>;
+  auto matrix = std::mdspan<T, view_2d, std::layout_right>(vec.data(), n, n);
 
-    using view_2d = std::extents<T, std::dynamic_extent, std::dynamic_extent>;
-    auto matrix = std::mdspan<T, view_2d, std::layout_right>(vec.data(), n,n);
-    
-    Multiply multiplier;
-    // Decomposing a matrix into Lower Triangular
-    for (int i = 0; i < matrix.extent(0); i++) {
-        for (int j = 0; j <= i; j++) {
-            int sum = 0;
- 
-            if (j == i) // summation for diagonals
-            {
+  Multiply multiplier;
+  // Decomposing a matrix into Lower Triangular
+  for (int i = 0; i < matrix.extent(0); i++) {
+    for (int j = 0; j <= i; j++) {
+      int sum = 0;
 
-                // apply transform_reduce for the first [lower[j].cbegin(), lower[j].cbegin() +j ) 
-                // sum = std::transform_reduce(std::execution::par, lower[j].cbegin(), lower[j].cbegin() +j, 0, std::plus{}, Square());
+      if (j == i)  // summation for diagonals
+      {
 
-                sum = std::transform_reduce(std::execution::par, lower[j].cbegin(), lower[j].cbegin() +j, 
-                    0, std::plus{},
-                        [=, i = i, j = j](int n) { return n * n; }); 
+        // apply transform_reduce for the first [lower[j].cbegin(), lower[j].cbegin() +j )
+        // sum = std::transform_reduce(std::execution::par, lower[j].cbegin(), lower[j].cbegin() +j, 0, std::plus{}, Square());
 
-                lower[j][j] = std::sqrt(matrix(i,j) - sum);
+        sum = std::transform_reduce(std::execution::par, lower[j].cbegin(),
+                                    lower[j].cbegin() + j, 0, std::plus{},
+                                    [=, i = i, j = j](int n) { return n * n; });
 
-            } else {
-                // Evaluating L(i, j) using L(j, j)
+        lower[j][j] = std::sqrt(matrix(i, j) - sum);
 
-                sum = std::transform_reduce(std::execution::par, lower[j].cbegin(),
-                                              lower[j].cbegin() + j,
-                                              lower[i].cbegin(),
-                                              0,
-                                              std::plus<>(),
-                                              multiplier);
+      } else {
+        // Evaluating L(i, j) using L(j, j)
 
-                lower[i][j] = (matrix(i,j) - sum) / lower[j][j];
-            }
-        }
+        sum = std::transform_reduce(std::execution::par, lower[j].cbegin(),
+                                    lower[j].cbegin() + j, lower[i].cbegin(), 0,
+                                    std::plus<>(), multiplier);
+
+        lower[i][j] = (matrix(i, j) - sum) / lower[j][j];
+      }
     }
- 
-    // Displaying Lower Triangular and its Transpose
-    cout << setw(6) << " Lower Triangular" 
-         << setw(30) << "Transpose" << endl;
-    for (int i = 0; i < n; i++) {
-         
-        // Lower Triangular
-        for (int j = 0; j < n; j++)
-            cout << setw(6) << lower[i][j] << "\t";
-        cout << "\t";
-         
-        // Transpose of Lower Triangular
-        for (int j = 0; j < n; j++)
-            cout << setw(6) << lower[j][i] << "\t";
-        cout << endl;
-    }
+  }
+
+  // Displaying Lower Triangular and its Transpose
+  cout << setw(6) << " Lower Triangular" << setw(30) << "Transpose" << endl;
+  for (int i = 0; i < n; i++) {
+
+    // Lower Triangular
+    for (int j = 0; j < n; j++)
+      cout << setw(6) << lower[i][j] << "\t";
+    cout << "\t";
+
+    // Transpose of Lower Triangular
+    for (int j = 0; j < n; j++)
+      cout << setw(6) << lower[j][i] << "\t";
+    cout << endl;
+  }
 }
- 
-// Driver Code for testing
-int main()
-{
-    constexpr int n = 3;
-    std::vector<int> v{4, 12, -16,12, 37, -43, -16, -43, 98 };
-    Cholesky_Decomposition(v, n);
 
-    return 0;
+// Driver Code for testing
+int main() {
+  constexpr int n = 3;
+  std::vector<int> v{4, 12, -16, 12, 37, -43, -16, -43, 98};
+  Cholesky_Decomposition(v, n);
+
+  return 0;
 }
